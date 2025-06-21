b51673e8ff497f6441681cc3e8a2978bd8a274d3
“cryspycreme” <11529@holbertonstudents.com>
2025-06-21 08:32:16 +0000

    Add program 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs- computes the absolute value of an integer
+ *
+ * Description: computes the absolute value of an integer
+ * @int: interger to be checked
+ * Return: if i < 0, return -i; else return i
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (i * -1);
+	}
+	else
+	{
+		return (i);
+	}
+}
