09fe36c1127e90de47b75fe0407ce7d952f92049
“cryspycreme” <11529@holbertonstudents.com>
2025-06-22 06:45:09 +0000

    Update betty style doc for 11-print_to_98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  * print_to_98 - return code.
  *
  * Description: prints all natural numbers from n to 98.
- * @n: the number passed to the function
+ * @n: the number to be passed to the function
  *
  * Return: void.
  */
@@ -17,18 +17,16 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		while (n < 98)
-	
 	{
 		printf("%d, ", n++);
 	}
 	printf("%d\n", n);
 	}
-	else 
+	else
 	{
 		while (n > 98)
 	{
 		printf("%d, ", n--);
-		
 	}
 	printf("%d\n", n);
 	}
