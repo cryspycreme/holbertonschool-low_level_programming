2a0278d47928e47f3c2a7065e6328a222694a879
“cryspycreme” <11529@holbertonstudents.com>
2025-06-22 10:45:10 +0000

    Update betty style doc 9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 
 /**
  * times_table - prints the 9 times tables
- * 
+ *
  * Description: prints 9 times tables starting at 0.
  * Return: void.
  */
@@ -13,8 +13,8 @@ void times_table(void)
 {
 
 	int x;
-	int y;
-	
+	int y = 0;
+
 	while (y <= 9)
 	{
 		x = 0;
@@ -23,7 +23,7 @@ void times_table(void)
 		int result;
 
 	result = (x * y);
-	
+
 	if (result <= 9)
 	{
 		if (x != 0)
@@ -37,14 +37,14 @@ void times_table(void)
 	_putchar((result / 10) + '0');
 	_putchar((result % 10) + '0');
 	}
-	
+
 	if (x != 9)
 	{
 	_putchar(',');
 	_putchar(' ');
 	}
 	x++;
-	}	
+	}
 	_putchar('\n');
 	y++;
 	}
