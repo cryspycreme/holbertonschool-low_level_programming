e6588d1f7d87d02b6534969fcb7a2470beb5907a
“cryspycreme” <11529@holbertonstudents.com>
2025-06-22 09:04:46 +0000

    Updating 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 
 /**
  * times_table - prints the 9 times tables
- *
+ * 
  * Description: prints 9 times tables starting at 0.
  * Return: void.
  */
@@ -49,4 +49,3 @@ void times_table(void)
 	y++;
 	}
 }
-
