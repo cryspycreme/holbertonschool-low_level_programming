2d24ccac3758656b0873ecc2b4d49de2cbc478e2
“cryspycreme” <11529@holbertonstudents.com>
2025-06-22 05:27:44 +0000

    Add program 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - returns code
+ *
+ * Description: returns the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
