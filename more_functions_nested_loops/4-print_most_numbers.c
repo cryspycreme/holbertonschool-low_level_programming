1146edb106392c9a39b01a6b7e0fc377725c6e1a
“cryspycreme” <11529@holbertonstudents.com>
2025-06-23 03:31:30 +0000

    Add program 4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints numbers from 0 to 9, except 2 and 4
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i = i + 1;
+	}
+	_putchar('\n');
+}
