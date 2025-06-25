83de43ea0b25df4d5f0a915ed47c1067cf01272e
“cryspycreme” <11529@holbertonstudents.com>
2025-06-25 07:06:55 +0000

    Add program 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* puts2- prints every other character of a string, starting with the first character
+*
+* @str: pointer that prints to a location which stores a string of characters
+* Return: void
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+		_putchar(str[i]);
+		}
+		else
+		{
+
+		}
+	}
+	_putchar('\n');
+}
