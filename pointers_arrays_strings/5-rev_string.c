0febfefe661c9261227d64d61dd46c1c51ef96e0
“cryspycreme” <11529@holbertonstudents.com>
2025-06-26 08:56:03 +0000

    Updated 5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,29 +1,34 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
-* rev_string: reverses the string
+* rev_string- reverses a string
 *
-* @s: pointer to a location that stores a string of characters
+* @s: pointer to the string
 * Return: void
 */
 
 void rev_string(char *s)
 {
-	/*temp array to store elements in reverse order*/
-	int temp[10];
+    /*temp array to store elements in reverse order*/
+    int temp[10];
 
-	int i; /*loop counter*/
-	
-	/*Copy elements from original array to temp in reverse order*/
-	for (i = 0; i < 10; i++)
-	{
-		temp[i] = s[10 - i - 1];
-	}
+    int i; /*loop counter*/
 
-	/*Copy elements back to original array*/
-	for (i = 0; i < 10; i++)
-	{
-		s[i] = temp[i];
-	}
+    /*Copy elements from original array to temp in reverse order*/
+    for (i = 0; i < 10; i++)
+    {
+        temp[i] = s[10 - 2 - i];
+        /* printf("%c", temp[i]);*/
+    }
+
+    /*Copy elements back to original array*/
+    for (i = 0; i < 10; i++)
+    {
+        /* printf("%c", s[i]);*/
+        /* printf(" ");*/
+        /* printf("%c", temp[i]);*/
+        /* printf("\n");*/
+
+        s[i] = temp[i];
+    }
 }
