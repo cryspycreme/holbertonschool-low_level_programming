e73293d0f7a3fe84ac7368e4d291cee3894e85dd
“cryspycreme” <11529@holbertonstudents.com>
2025-06-26 05:57:51 +0000

    Update 4-print_rev.c for betty style coding

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -18,9 +18,9 @@ void print_rev(char *s)
 		/*length increases until it hits the end of the string*/
 		/*do not reset value of length as this will be used in next for loop*/
 	}
-	
+
 	for (i = length - 1; i >= 0; i--)
-	{		
+	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
