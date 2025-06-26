3428bebcddb2f31054b06c94365917fefc7533b8
“cryspycreme” <11529@holbertonstudents.com>
2025-06-26 05:59:39 +0000

    Add betty style doc to 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* puts2- prints every other character of a string, starting with the first character
+* puts2- prints every other character of a string
 *
 * @str: pointer that prints to a location which stores a string of characters
 * Return: void
