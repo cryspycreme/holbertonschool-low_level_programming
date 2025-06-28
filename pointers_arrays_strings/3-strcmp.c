95ab0f1a707d27f15f36c7991c16541b5a629500
“cryspycreme” <11529@holbertonstudents.com>
2025-06-28 08:11:20 +0000

    Updated code to fit checker req

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,17 +12,20 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int result;
 
 	i = 0;
 
-	if (s1[i] == s2[i])
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		result = 0;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return (0);
+		}
+	i++;	
 	}
-	else
-	{
-		result = (s1[i] - s2[i]);
-	}
-	return (result);
+	return (0);
 }
