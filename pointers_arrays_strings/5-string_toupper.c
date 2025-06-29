1f5006614214d1f40cce235925706c7ae6128552
“cryspycreme” <11529@holbertonstudents.com>
2025-06-29 01:46:26 +0000

    Update 5-string_toupper.c for betty coding style

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -6,19 +6,20 @@
 * @str: pointer to string
 * Return: character string
 */
+
 char *string_toupper(char *str)
 {
-    int i; /*loop counter*/
+	int i; /*loop counter*/
 
-    i = 0;
+	i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 97 && str[i] <= 122)
-        {
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
 			str[i] = (str[i] - 32);
-        }
-        i++;
-    }
+		}
+	i++;
+	}
 	return (str);
 }
