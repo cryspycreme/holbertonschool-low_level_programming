61d09c72fc2c079eb7dafe704c12015c23570214
“cryspycreme” <11529@holbertonstudents.com>
2025-07-01 02:31:25 +0000

    Add program 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _strstr- finds the first occurrence of the substring in haystack
+* @haystack: pointer to the string
+* @needle: pointer to the substring to be found
+* Return: pointer to haystack where substring is located.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	while (needle[i] != '\0')
+	{
+		j = 0;
+
+		while (haystack[j] != '\0')
+		{
+			if (haystack[j] == needle[i])
+			{
+				return (haystack + j);
+			}
+		j++;
+		}
+	i++;
+	}
+return (0);
+}
