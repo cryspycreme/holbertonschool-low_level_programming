412b7aaf40ff73476073eeedccfc0b3c6bd67c27
“cryspycreme” <11529@holbertonstudents.com>
2025-07-02 01:47:08 +0000

    Moved files to recursion directory

---
+++ recursion/README.md
@@ -0,0 +1,6 @@
+C - Recursion
+
+0-puts_recursion.c: Write a function that prints a string, followed by a new line.
+
+
+This repo is maintained by cryspycreme.
---
+++ recursion/main.h
@@ -0,0 +1,9 @@
+#ifndef recursion_headers
+#define recursion_headers
+
+/*Function Declarations*/
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif
