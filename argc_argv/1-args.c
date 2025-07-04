8d23e76eba6e92eb596e2ebe406282e2cc21462f
“cryspycreme” <11529@holbertonstudents.com>
2025-07-04 02:57:25 +0000

    Update for betty style doc

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,12 +4,12 @@
 /**
  * main - Entry point
  *
- * @argsc: no. of arguments passed into function
- * @argsv: strings of arguments passed into function
+ * @argc: no. of arguments passed into function
+ * @argv: strings of arguments passed into function
  * Return: always 0 (Success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
 	(void)argv;
