b4707afcd38fa43cbe33b8f5fe49b2b5d903e3f3
“cryspycreme” <11529@holbertonstudents.com>
2025-07-04 02:54:27 +0000

    Add program 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: count of arguments
+ * @argv: arguments passed as strings
+ * Return: Always 0 (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	
+	return (0);
+}
+
