0605557fd982099d239f7268d45f39bef714ef6f
“cryspycreme” <11529@holbertonstudents.com>
2025-07-04 08:02:35 +0000

    Add program 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -15,23 +15,28 @@ int main(int argc, char *argv[])
 {
 	int i = 1;
 	int sum = 0;
+	int j = 0;
 
 	if (argc <= 1)
 	{
 		printf("%d\n", 0);
+		return (0);
 	}
 
 	while (i < argc)
 	{
-		if (!isdigit[argv[i])
+		j = 0;
+
+		while (argv[i][j] != '\0')
 		{
-			printf("Error\n");
-                        return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		j++;
 		}
-		else
-		{
-			sum = atoi(argv[i]) + sum;
-		}
+	sum = atoi(argv[i]) + sum;
 	i++;
 	}
 printf("%d\n", sum);
