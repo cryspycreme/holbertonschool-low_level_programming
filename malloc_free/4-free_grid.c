7338a5399baa4d594884416449669419dc5fab32
“cryspycreme” <11529@holbertonstudents.com>
2025-07-07 11:45:55 +0000

    Add program 4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*free_grid - frees 2D grid previously created
+*
+*@grid: the array
+*@height: height of the grid
+*Return: void
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
