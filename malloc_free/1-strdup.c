bfe1f0733a933b47cee6491abe44906a184e5e0e
“cryspycreme” <11529@holbertonstudents.com>
2025-07-07 04:27:28 +0000

    Add program 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup- returns pointer to newly allocated space in memory
+ *
+ * @str: the string to be duplicated
+ * Return: pointer to duplicated string, or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	/*find the length of str*/
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	/*allocate memory for duplicate string*/
+	dup = malloc(sizeof(*dup) * i);
+
+	/*duplicate string*/
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (dup == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			dup[i] = str[i];
+		}
+	i++;
+	}
+	return (dup);
+}
