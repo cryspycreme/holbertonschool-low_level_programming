37dee6d4599d23eb95dc284288aa20121f0d3310
“cryspycreme” <11529@holbertonstudents.com>
2025-07-09 03:17:57 +0000

    Drafted all files for 2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc- allocates memory for an array and initialises memory to 0.
+ *
+ * @nmemb: no. of elements in array
+ * @size: byte size of each element
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
