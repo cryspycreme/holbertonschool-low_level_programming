89517d1b6acdbef6689b861a1dc5588bdf021777
“cryspycreme” <11529@holbertonstudents.com>
2025-07-10 06:33:18 +0000

    Updated code for efficiency

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,13 +12,31 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		exit(0);
+		return;
 	if (d->name == NULL)
-		printf("Name: (nil)\nAge: %f\nOwner: %s\n", d->age, d->owner);
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	
 	if (d->age == 0)
-		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
+        {
+                printf("Age: (nil)\n");
+        }
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	
 	if (d->owner == NULL)
-		printf("Name: %s\nAge: %f\nOwner: (nil)", d->name, d->age);
-
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+        {
+                printf("Owner: (nil)\n");
+        }
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
