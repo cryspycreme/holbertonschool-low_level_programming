0c834d661ba8c90ec3eacccb42c064d1e28abe90
“cryspycreme” <11529@holbertonstudents.com>
2025-07-10 05:37:03 +0000

    Add program 1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog- initialise the variable of type struct dog
+ *
+ * @d: pointer to the struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner's name
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
