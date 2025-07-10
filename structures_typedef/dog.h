1097701f995d7298b54b63ec238cd4db3b9bb33b
“cryspycreme” <11529@holbertonstudents.com>
2025-07-10 04:38:51 +0000

    Updated error in header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef struct_dog_header
 #define struct_dog_header
 
+/**
+ * struct dog - structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner's name
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
