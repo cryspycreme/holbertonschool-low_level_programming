ed47f3feb6b3e6fb67a06fa79e027dcfdc1eb74d
“cryspycreme” <11529@holbertonstudents.com>
2025-07-12 02:54:34 +0000

    Add program 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,40 +3,54 @@
 #include <stdlib.h>
 
 /**
- * new_dog: creates a new dog
+ * new_dog- creates a new dog
  *
  * @name: pointer to a copied location of the name
  * @age: age of dog
  * @owner: pointer to a copied location of the owner's name
- * Return: pointer to new dog_t structure, or NULL
+ * Return: pointer to new_dog structure, or NULL
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i,n;
-	
-	new_dog = malloc(sizeof(dog_t);
+	int i = 0, j = 0, name_len = 0, own_len = 0;
+
+	dog_t *new_dog = malloc(sizeof(dog_t));
 		if (new_dog == NULL)
 			return (NULL);
-	
-	/*find length of name*/
-	n = 0;
-	
-	while (name[n] != '\0')
-	{
-		n++;
-	}
-	/*allocate and copy name*/
-	new_dog->name = malloc(sizeof(char) * n))
+
+	while (name[name_len] != '\0')
+		name_len++;
+	new_dog->name = malloc(sizeof(char) * name_len + 1);
 		if (new_dog->name == NULL)
+		{
 			free(new_dog);
 			return (NULL);
-	
-	i = 0;
-	while (i < n)
+		}
+
+	while (i < name_len)
 	{
 		new_dog->name[i] = name[i];
+		i++;
 	}
 
+	new_dog->age = age;
 
+	while (owner[own_len] != '\0')
+		own_len++;
+
+	new_dog->owner = malloc(sizeof(char) * own_len + 1);
+		if (new_dog->owner == NULL)
+		{
+			free(new_dog->name);
+			free(new_dog);
+			return (NULL);
+		}
+
+	while (j < own_len)
+	{
+		new_dog->owner[j] = owner[j];
+		j++;
+	}
+return (new_dog);
 }
