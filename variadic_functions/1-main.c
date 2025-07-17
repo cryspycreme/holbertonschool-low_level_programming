c19e89fdfc9130c28d7a3ea523882245458a2cfe
“cryspycreme” <11529@holbertonstudents.com>
2025-07-17 05:10:31 +0000

    Draft 1-print_numbers.c

---
+++ variadic_functions/1-main.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers- prints numbers, followed by \n
+ *
+ * @separator: string separator to be printed between no.s
+ * @n: no. of integers passed to the function
+ * Return: void. 
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+}
