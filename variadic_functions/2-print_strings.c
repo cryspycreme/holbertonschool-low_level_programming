5574bf3d662254bbbb3f422172da9faec1ee77ec
“cryspycreme” <11529@holbertonstudents.com>
2025-07-17 06:35:48 +0000

    Add program 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,4 +10,26 @@
  * Return: void.
  */
 
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
 
+	va_list strings;
+
+	va_start(strings, n);
+
+	while (i < n)
+	{
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s%s", va_arg(strings, char *), separator);
+		}
+		else
+		{
+			printf("%s", va_arg(strings, char *));
+		}
+		i++;
+	}
+	va_end(strings);
+	printf("\n");
+}
