e97a45fff4e94865657e371c43fffd350c7a2d71
“cryspycreme” <11529@holbertonstudents.com>
2025-07-17 05:53:34 +0000

    Update for betty style doc

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	
+
 	while (i < n)
 	{
 		sum = sum + va_arg(num_args, int);
---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings- prints strings, followed by \n
+ *
+ * @separator: string separator to be printed between strings
+ * @n: no. of strings passed to the function
+ * Return: void.
+ */
+
+
