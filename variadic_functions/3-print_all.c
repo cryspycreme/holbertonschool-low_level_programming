1f222d871675b2a31144d37c60117cde9b13782c
“cryspycreme” <11529@holbertonstudents.com>
2025-07-20 07:18:08 +0000

    Add program 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -67,6 +67,7 @@ void print_all(const char *const format, ...)
 	va_list args;
 	int i = 0, j = 0;
 	char *separator = "";
+
 	va_start(args, format);
 
 	while (format != NULL && format[i] != '\0')
