8a4d341e85b35aa5b1a21165fc490e93448318ab
“cryspycreme” <11529@holbertonstudents.com>
2025-07-29 05:58:49 +0000

    Add program 2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+* add_node - adds a new node to a singly linked list
+* @head: pointer to a pointer to the head of linked list
+* @str: string to be copied
+*
+* Return: a pointer to the new node added (ie. list_t strcut)
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(new_node->str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
