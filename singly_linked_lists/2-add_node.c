18e52f5b58839b9bc5e4fc99094bb1d0fd412e92
“cryspycreme” <11529@holbertonstudents.com>
2025-07-29 06:18:46 +0000

    Fix betty style

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,10 +15,10 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 	int i = 0;
-	
+
 	if (new_node == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	new_node->str = strdup(str);
 	while (str[i] != '\0')
 	{
