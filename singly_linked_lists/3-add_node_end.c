f783eda4aadf88ea6268b88a1a79bb0787918160
“cryspycreme” <11529@holbertonstudents.com>
2025-07-30 10:30:20 +0000

    Fixed betty style

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -19,12 +19,12 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	while (str[i] != '\0')
