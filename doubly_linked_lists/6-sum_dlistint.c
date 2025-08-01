e0554f258930a08dcec10f8b96e19b3a7f5634d6
“cryspycreme” <11529@holbertonstudents.com>
2025-08-01 06:25:31 +0000

    Add program 6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* sum_dlistint- returns the sum of all the data (n) of the linked list.
+*
+* @head: pointer to list
+*
+* Return: sum of data in each node.
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	while (current->next != NULL)
+	{
+		if (current == NULL)
+		{
+			return (0);
+		}
+		sum = sum + current->n;
+		current = current->next;
+	}
+	sum = sum + current->n;
+	return (sum);
+}
