11bfc9d488a4b718b7f965f1cdb184588a2b5314
“cryspycreme” <11529@holbertonstudents.com>
2025-08-01 06:01:12 +0000

    Fix betty styles

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
 
 /**
 * get_dnodeint_at_index- returns the nth node of a dlistint_t linked list.
-* 
+*
 * @head: pointer to list
 * @index: index of the node, starting from 0.
 * Return: the nth node of a dlistint_t linked list.
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current = head;
 	unsigned int count = 0;
-	
+
 	while (count < index)
 	{
 		if (current == NULL)
