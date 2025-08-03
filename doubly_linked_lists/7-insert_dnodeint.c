d7612f1c5967957838ad6b73e62247d80b7f3dbb
“cryspycreme” <11529@holbertonstudents.com>
2025-08-03 08:35:52 +0000

    Fix code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -40,13 +40,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	while (prev_count < idx - 1)
 	{
+		if (node_prev == NULL) /*index out of range*/
+        	{
+                	free(new_node);
+                	return (NULL);
+        	}
 		node_prev = node_prev->next;
 		prev_count++;
-	}
-	if (node_prev == NULL) /*index out of range*/
-	{
-		free(new_node);
-		return (NULL);
 	}
 	new_node->next = node_prev->next;
 	new_node->prev = node_prev;
