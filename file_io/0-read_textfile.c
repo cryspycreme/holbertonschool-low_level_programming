9ad3232939f7d07ed1c0183bda1ec2cd647c96c1
“cryspycreme” <11529@holbertonstudents.com>
2025-08-26 07:18:42 +0000

    Update mode options

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     ssize_t bytesRead, bytesW;
 
     /*open the file*/
-    file_desc = open(filename, O_RDWR);
+    file_desc = open(filename, O_RDWR | O_RDONLY);
         if (file_desc == -1)
 	      return (0);
 
