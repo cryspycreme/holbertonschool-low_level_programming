75517778749135ceeef57315b7cd4e5c50153332
“cryspycreme” <11529@holbertonstudents.com>
2025-08-26 07:45:03 +0000

    Fix betty styles and doc

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,41 +5,44 @@
 
 #define BUF_SIZE 10000
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-
+/**
+ * read_textfile- reads a text file and prints it to stdout.
+ *
+ * @filename: the name of file to be read
+ * @letters: no. of letters to read/print
+ * Return: no. letters read/printed
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_desc;
-    char buf[BUF_SIZE];
-    ssize_t bytesRead, bytesW;
+	int file_desc;
+	char buf[BUF_SIZE];
+	ssize_t bytesRead, bytesW;
 
-    /*open the file*/
-    if (filename == NULL)
-            return (0);
-    
-    file_desc = open(filename, O_RDONLY, S_IRUSR);
-        if (file_desc == -1)
-	      return (0);
+	/*open the file*/
+	if (filename == NULL)
+		return (0);
 
-    if (file_desc != -1)
-    {
-	/*read the file*/
-	    bytesRead = read(file_desc, buf, letters);
+	file_desc = open(filename, O_RDONLY, S_IRUSR);
+	if (file_desc == -1)
+		return (0);
 
-        /*if read fails*/
-        if (bytesRead == -1)
-            return (0);
+	if (file_desc != -1)
+	{
+		/*read the file*/
+		bytesRead = read(file_desc, buf, letters);
 
-        /*print the file to output*/
-        bytesW = write(STDOUT_FILENO, buf, bytesRead);
+		/*if read fails*/
+		if (bytesRead == -1)
+			return (0);
 
-        /*if write fails*/
-        if (bytesW == -1)
-            return (0);
-    }
-   close (file_desc);
-    return (bytesW); 
+		/*print the file to output*/
+		bytesW = write(STDOUT_FILENO, buf, bytesRead);
+
+		/*if write fails*/
+		if (bytesW == -1)
+			return (0);
+	}
+	close(file_desc);
+	return (bytesW);
 }
