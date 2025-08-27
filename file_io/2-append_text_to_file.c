61684dc5da4b97048381826185aa22fe348f319a
“cryspycreme” <11529@holbertonstudents.com>
2025-08-27 04:22:05 +0000

    Remove O_CREAT flag

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_desc = open(filename, O_RDWR | O_CREAT | O_APPEND, 0664);
+	file_desc = open(filename, O_RDWR | O_APPEND, 0664);
 	if (file_desc == -1)
 		return (-1);
 
