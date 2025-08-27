af5bc742ffe63637cca86e296100c851fa5b513b
“cryspycreme” <11529@holbertonstudents.com>
2025-08-27 02:26:16 +0000

    Fix betty styles

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,7 +6,7 @@
 
 /**
  * create_file - function that creates a file
- * 
+ *
  * @filename: name of file to create
  * @text_content: null terminated string to write to the file
  * Return: 1 on success, -1 on failure.
@@ -18,11 +18,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	file_desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file_desc == -1)
 		return (-1);
-	
+
 	/*write to file*/
 	if (text_content != NULL)
 	{
