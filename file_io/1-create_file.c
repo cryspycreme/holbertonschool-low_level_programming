d34593c333e9043ae6104a46d645b4591185e2a6
“cryspycreme” <11529@holbertonstudents.com>
2025-08-27 02:25:29 +0000

    Update code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,24 +4,32 @@
 #include <string.h>
 #include "main.h"
 
+/**
+ * create_file - function that creates a file
+ * 
+ * @filename: name of file to create
+ * @text_content: null terminated string to write to the file
+ * Return: 1 on success, -1 on failure.
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int file_desc, bytesW;
-	
+
 	if (filename == NULL)
 		return (-1);
-		
+	
 	file_desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file_desc == -1)
-		return (0);
-		
+		return (-1);
+	
 	/*write to file*/
 	if (text_content != NULL)
 	{
 		bytesW = write(file_desc, filename, (strlen(text_content) + 1));
 		if (bytesW == -1)
-			return (0);
+			return (-1);
 	}
 	close(file_desc);
-	return (0);
+	return (1);
 }
