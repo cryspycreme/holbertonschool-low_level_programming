d8d4a0915b34a122358e2fbc0e83426856a2673d
“cryspycreme” <11529@holbertonstudents.com>
2025-08-27 06:37:15 +0000

    Add extra error check for read

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -72,20 +72,16 @@ int main(int argc, char *argv[])
 	if (fd_file_to == -1)
 		print_error(99, file_to, 0);
 
-	bytesR = read(fd_file_from, cp_ff, 1024);
-	if (bytesR == -1)
-                print_error(98, file_from, 0);
-
-	while (bytesR > 0)
+	while (bytesR = read(fd_file_from, cp_ff, 1024) != 0)
 	{
+		if (bytesR == -1)
+                	print_error(98, file_from, 0);
+		
 		bytesW = write(fd_file_to, cp_ff, bytesR);
 
 		if (bytesW == -1)
 			print_error(99, file_to, 0);
 	}
-	if (bytesR == -1)
-		print_error(98, file_from, 0);
-
 	close_ff = close(fd_file_from);
 	if (close_ff == -1)
 		print_error(100, NULL, fd_file_from);
