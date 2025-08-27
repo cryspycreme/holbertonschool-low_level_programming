bc5d377aa764e49572edc95b6eac9a2a8e174205
“cryspycreme” <11529@holbertonstudents.com>
2025-08-27 02:40:11 +0000

    Fix ebtty styles

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	file_desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file_desc == -1)
 		return (-1);
-	
+
 	/*write to file*/
 	if (text_content != NULL)
 	{
