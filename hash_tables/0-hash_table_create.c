a6d8e074c69c29c723a97389a5aaaff9ab95d1dc
“cryspycreme” <11529@holbertonstudents.com>
2025-08-29 02:13:34 +0000

    Change size of hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
 
-	hash_t = malloc(sizeof(hash_table_t) * size);
+	hash_t = malloc(sizeof(hash_t) * size);
 	if (hash_t == NULL)
 		return (NULL);
 
