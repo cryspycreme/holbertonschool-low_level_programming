f7bd675fcdfb43bdf6ceebbadf2195a2a8a9281b
“cryspycreme” <11529@holbertonstudents.com>
2025-08-29 08:11:47 +0000

    Add program 6-hash_table_delete

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table
+ *
+ * Return: nothing.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *tmp;
+	
+	if (ht == NULL)
+		return;
+	
+	i = 0;
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tmp = node;
+			node = node->next;
+      			free(tmp->key);
+      			free(tmp->value);
+			free(tmp);
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
