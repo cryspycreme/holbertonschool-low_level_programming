21e963ab90ca500279adc2abd35a1885da0d736d
“cryspycreme” <11529@holbertonstudents.com>
2025-08-29 02:56:19 +0000

    Fix betty doc

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,14 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
+/**
+ * key_index- returns the index of each key
+ *
+ * @key: the key
+ * @size: size of the hash table
+ * Return: index where the key-value pair is stored.
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
