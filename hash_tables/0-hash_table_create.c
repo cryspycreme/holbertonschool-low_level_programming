399e6805cbdf90ec80982e10155ce430ac6b2adc
“cryspycreme” <11529@holbertonstudents.com>
2025-08-29 02:37:57 +0000

    Fixed malloc sizing of the array table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,13 +12,34 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
+	hash_node_t *node_ptr;
+	unsigned long int i;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
-	hash_t = malloc(sizeof(hash_t) * size);
+	hash_t = malloc(sizeof(hash_table_t));
 	if (hash_t == NULL) 
 		return (NULL);
+
+	/*set the size field of struct*/
+	hash_t->size = size;
+
+	/*allocate memory for the array of bucket pointers*/
+	hash_t->array = malloc(sizeof(node_ptr) * size);
+	if (hash_t->array == NULL)
+	{
+		free(hash_t);
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (i < size)
+	{
+		hash_t->array[i] = NULL;
+		i++;
+	}
 
 	return (hash_t);
 }
