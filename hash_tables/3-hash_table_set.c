46794f748147f63509b9f0c0b92f352fae78819d
“cryspycreme” <11529@holbertonstudents.com>
2025-08-29 04:18:56 +0000

    Fix betty styles

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	/*determine index of key*/
-        index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	node = ht->array[index];
 
