16197cb5083be3ad188c6430c07a6a6dc46639b4
“cryspycreme” <11529@holbertonstudents.com>
2025-08-29 05:49:34 +0000

    Add program 4-has_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table you want to look into
+ * @key: the key that we are searching for
+ *
+ * Return: the value associated with the key, or NULL if key not found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if ((ht == NULL) || (key == NULL) || (key[0] == '\0'))
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+
+	/*look inside the bucket at index 'x'*/
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (strdup(node->value));
+	}
+	return (NULL);
+}
